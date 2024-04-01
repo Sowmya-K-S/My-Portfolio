a0fa7d9950ca65f19b4d8ad6343bb628e5a07733
Sowmya K S <119122284+Sowmya-K-S@users.noreply.github.com>
2024-04-02 00:26:24 +0530

    home half done - footer done - added many small features - need to complete home and contact form and add AOS

---
+++ script.js
@@ -0,0 +1,36 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+          const offsetTop = targetElement.offsetTop;
+          const windowHeight = window.innerHeight;
+          const scrollTo = offsetTop - (windowHeight / 2) + (targetElement.offsetHeight / 2);
+          window.scrollTo({ top: scrollTo, behavior: 'smooth' });
+      }
+  });
+});
+
+
+ // Function to check scroll position and toggle button visibility
+ window.onscroll = function() {
+  toggleGoToTopButton();
+};
+
+function toggleGoToTopButton() {
+  var button = document.getElementById("goToTopBtn");
+  // If the user has scrolled down, show the button; otherwise, hide it
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      button.style.display = "block";
+  } else {
+      button.style.display = "none";
+  }
+}
+
+// Function to scroll to the top of the page
+function goToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
